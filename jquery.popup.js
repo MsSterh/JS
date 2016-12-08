c7cdd728a77f692a5cd0b2dbac62b65e5ebbdb16
MsSterh <ms.sterh@gmail.com>
2016-12-08 11:01:14 +0200

    Update

--- jquery.popup.js
+++ jquery.popup.js
@@ -1,7 +1,9 @@
 // set the same 'name' for next tags:
 // [data-popup-link='name'] - for link which calls popup
 // [data-popup-content='name'] - for popup
-// [data-popup-close='name'] - for close popup button
+// [data-popup-ajax='true'] - for ajax popups
+// [data-popup-position='left'] - set status as class 'isLeft'
+// [data-popup-close='true'] - for close popup button
 
 (function($) {
   $.fn.popup = function(options) {
@@ -12,33 +14,58 @@
           } else {
             $('.PopupOverlay').removeClass('isHidden');
           }
+
+          if ($('.PopupContainer').length === 0) {
+            $("body").append("<div class='PopupContainer'></div>");
+          } else {
+            $('.PopupContainer').removeClass('isHidden');
+          }
+          $('body').addClass('PopupOpen');
         },
 
-      $popup = function(name) { return  $('[data-popup-content=' + name + ']'); },
-      $close = function(name) { return  $('[data-popup-close=' + name + ']');   };
+      $link = function(name) { return  $('[data-popup-link=' + name + ']'); },
+      $popup = function(name) { return  $('[data-popup-content=' + name + ']'); };
 
-    var closePopup = function(name) {
-          $popup(name).hide();
+    var closeAllPopups = function() {
+          $('[data-popup-content]').hide();
           $('.PopupOverlay').unbind('click').addClass('isHidden');
+          $('.PopupContainer').addClass('isHidden');
           $(document).unbind('keyup');
+          $('body').removeClass('PopupOpen');
         };
 
     var bindClosePopup = function(name) {
           // close by clicking on Overlay
           $('.PopupOverlay').bind('click', function() {
-            closePopup(name);
+            closeAllPopups();
           });
           // close by ESC pressing
           $(document).on('keyup', function(e) {
-            (e.keyCode == 27) && (closePopup(name));
+            (e.keyCode == 27) && (closeAllPopups());
           });
         };
 
-    var closePopupByButton = function(name) {
-      if ($close(name).length > 0) {
-        $close(name).on('click', function() {
-          closePopup(name);
+    var closePopupByButton = function() {
+      $("[data-popup-close='true']").on('click', function() {
+        closeAllPopups();
+      });
+    };
+
+    var setPopupPosition = function(name) {
+      var position = $link(name).data('popup-position');
+
+      if (position) {
+        var $content = $("[data-popup-content=" + name + "]"),
+            new_class = 'is' + position.substr(0, 1).toUpperCase() + position.substr(1);
+
+        $content.removeClass(new_class);
+        $content.position({
+          my: "right top",
+          at: "right top",
+          of: $("[data-popup-link=" + name + "]"),
+          collision: "none"
         });
+        $content.addClass(new_class);
       }
     };
 
@@ -60,15 +87,21 @@
               url: url,
               dataType: 'html'
             }).done(function(data) {
-              $this.after($(data).attr('data-popup-content', name));
-              closePopupByButton(name);
+              $(".PopupContainer").append($(data).attr('data-popup-content', name));
+              closePopupByButton();
+              setPopupPosition(name);
             });
           } else {
-            $popup(name).show();
+            var $loaded_popup = $popup(name);
+            if (!$loaded_popup.parent().hasClass('.PopupContainer')) {
+              $(".PopupContainer").append($loaded_popup);
+            }
+            $loaded_popup.show();
+            setPopupPosition(name);
           }
         });
 
-        closePopupByButton(name);
+        closePopupByButton();
     });
   };
 })(jQuery);
